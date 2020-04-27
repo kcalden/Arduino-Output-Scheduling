c217c55d49910e8d076d89a4c08fecb91f7a79b2
Karl Calden <33008330+kcalden@users.noreply.github.com>
2020-04-27 16:43:14 -0400

    Add getItemsLeft() and calcDelay()

--- PythonScheduler/sendinfo.py
+++ PythonScheduler/sendinfo.py
@@ -10,30 +10,43 @@ PRESCALER = 8
 FREQ = 16000000
 TIME_PER_CYCLE = PRESCALER/FREQ
 MAX_TIME = TIME_PER_CYCLE*0xFFFFFFFF
+
+# Send an output state to the device
 def digitalWrite(pin, state,delay):
     time_delay_bytelist = list(delay.to_bytes(4,'big'))
     sched_item_bytelist = [pin,state]
     sched_item_bytelist.extend(time_delay_bytelist)
-    # print(sched_item_bytelist)
+    print(sched_item_bytelist)
     port.write(bytearray(sched_item_bytelist))
-    
-pulses = 15
-## SCHEDULED
-# for x in range(int(pulses)):
-while(True):
-    digitalWrite(13,1,0xFFFFF)
-    time.sleep(0.4)
-    digitalWrite(13,0,0xFFFFF)
-    time.sleep(0.4)
 
-# digitalWrite(13,1,0xFFFFF)
 
-## LIVE
-# DUTY = 0.2
-# while(True):
-#     digitalWrite(13, 1, int(20*DUTY))
-#     digitalWrite(13,0, int(20*(1-DUTY)))
-#     time.sleep(0.02)
+# TODO: Make sure that we read in two bytes
+# Request items left 
+def getItemsLeft():
+    # Request number of items that can be written
+    port.write(bytearray([0x01]))
+    # Read bytes in from the serial port
+    # Convert the bytes into an unsigned integer
+    items_left_bytes = port.read(2)
+    return int.from_bytes(items_left_bytes, byteorder='big', signed=False)
+
+# Calculate counter value for the MCU
+def calcDelay(seconds):
+    if(seconds > MAX_TIME): return 0xFFFFFFFF
+    return int(seconds/TIME_PER_CYCLE)
+
+## TEST CODE ##
+items_left = 10
+
+digitalWrite(13, 1, calcDelay(0.25))
+digitalWrite(13, 0, calcDelay(0.25))
+digitalWrite(13, 1, calcDelay(0.25))
+digitalWrite(13, 0, calcDelay(0.25))
+# while(items_left > 0):
+#     digitalWrite(LED_BUILTIN, 1, 0xFFFFF)
+#     items_left = items_left - 1
+#     digitalWrite(LED_BUILTIN, 0, 0xFFFFF)
+#     items_left = items_left - 1
 
 while(True):
     pass
