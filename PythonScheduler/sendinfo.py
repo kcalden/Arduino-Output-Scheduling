a1a3e2e064b280c44b9267e6f4c89309ea161378
Karl Calden <33008330+kcalden@users.noreply.github.com>
2020-04-29 02:17:35 -0400

    Update test code for new packets

--- PythonScheduler/sendinfo.py
+++ PythonScheduler/sendinfo.py
@@ -13,24 +13,27 @@ MAX_TIME = TIME_PER_CYCLE*0xFFFFFFFF
 
 # Send an output state to the device
 def digitalWrite(pin, state,delay):
-    time_delay_bytelist = list(delay.to_bytes(4,'big'))
-    sched_item_bytelist = [pin,state]
-    sched_item_bytelist.extend(time_delay_bytelist)
+    # Signal output state write
+    packet = [0x02]
+
+    # Add pin and state
+    packet.extend([pin,state])
+
+    # Add time delay
+    packet.extend(list(delay.to_bytes(4,'big')))
+
+    # Zero pad packet
+    packet.extend([0])
     # print(sched_item_bytelist)
-    port.write(bytearray(sched_item_bytelist))
+    port.write(bytearray(packet))
 
-
-# TODO: Make sure that we read in two bytes
 # Request items left 
 def requestItemsLeft():
     # Request number of items that can be written
-    port.write(bytearray([0x01]))
-
+    port.write(bytearray([0x01,0x01,0,0,0,0,0,0]))
     # Read bytes in from the serial port and
     # Convert the bytes into an unsigned integer
     items_left_bytes = port.read(2)
-    # print(int.from_bytes(items_left_bytes, byteorder='big', signed=False))
-    time.sleep(0.2)
     return int.from_bytes(items_left_bytes, byteorder='big', signed=False)
 
 # Calculate counter value for the MCU
@@ -41,7 +44,7 @@ def calcDelay(seconds):
 def sendCMD(cmd_byte):
     port.write()
 
-# TEST LOOP ##
+# TEST BLINK ##
 def loop():
     items_left = requestItemsLeft()
     state = 1
