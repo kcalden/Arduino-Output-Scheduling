1dfc2096981ac5e0ebffe3e7cb85bf025229e665
Karl Calden <33008330+kcalden@users.noreply.github.com>
2020-04-29 22:57:34 -0400

    Add response and checksums

--- ArdunioScheduleHandler/src/main.cpp
+++ ArdunioScheduleHandler/src/main.cpp
@@ -12,6 +12,9 @@ StaticQueue<info_digitalout, SCHEDULE_LIMIT> write_schedule;
 // Interpret command
 void interpretCMDPacket(uint8_t packet[]);
 
+// Compute checksum
+uint8_t checksum(uint8_t packet[], unsigned int length);
+
 void setup() {
   // Initialize timers
   cli(); // Disable interrupts
@@ -23,15 +26,21 @@ void setup() {
 }
 
 // Eight byte packet buffer
-uint8_t packet_buf[8];
-
+uint8_t packet_buf[8], response_buffer[8];
 void loop() {
   while(Serial.available())
   {
+    // Clear response buffer
+    for(int i = 0;i<8;i++) response_buffer[i] = 0;
+
     // Read packet
     Serial.readBytes(packet_buf, 8);
 
+    // First bit is always the checksum
+    response_buffer[0] = checksum(packet_buf, 8);
+
     // Interpret packet
+    // TODO: Send response byte
     switch (packet_buf[0])
     {
       case 0x01: // Command
@@ -39,11 +48,22 @@ void loop() {
         break;
       case 0x02: // Output and delay packet
         info_digitalout new_info(packet_buf,1);
-
-        // If the buffer was empty, immediately trigger the interrupt
-        bool was_empty = write_schedule.empty();
-        write_schedule.push(new_info);
-        if(was_empty) set_TCNT1(1); 
+        // Check if the write schedule is full then push
+        if(!write_schedule.full()) {
+          // If the buffer was empty, immediately trigger the interrupt
+          bool was_empty = write_schedule.empty();
+          write_schedule.push(new_info);
+          if(was_empty) set_TCNT1(1);
+          Serial.write(response_buffer,8);
+        }else{
+          // Write back fault if the queue is full
+          response_buffer[1] = 0x01;
+          Serial.write(response_buffer,8);
+        }
+        break;
+      default:
+        response_buffer[0] = checksum(packet_buf, 8);
+        Serial.write(response_buffer, 8);
         break;
     }
   }
@@ -56,10 +76,23 @@ void interpretCMDPacket(uint8_t packet[])
   {
     case 0x01: // Memory remaining on the schedule
       unsigned int items_left = write_schedule.itemsLeft();
-      Serial.write((uint8_t)((items_left&0xFF00)>>8));
-      Serial.write((uint8_t)(items_left&0xFF));
+      response_buffer[1] = (uint8_t)((items_left&0xFF00)>>8);
+      response_buffer[2] = (uint8_t)(items_left&0xFF);
+      Serial.write(response_buffer,8);
       break;
   }
+}
+
+uint8_t checksum(uint8_t packet[], unsigned int length)
+{
+  unsigned int sum = 0;
+
+  for(int i = 0; i<length; i++)
+  {
+    sum += packet[i];
+  }
+
+  return (uint8_t) (sum & 0xFF);
 }
 
 ISR(TIMER1_OVF_vect)
--- PythonScheduler/sendinfo.py
+++ PythonScheduler/sendinfo.py
@@ -11,7 +11,30 @@ FREQ = 16000000
 TIME_PER_CYCLE = PRESCALER/FREQ
 MAX_TIME = TIME_PER_CYCLE*0xFFFFFFFF
 
+def checksum(byte_list):
+    sum = 0
+    for b in byte_list:
+        sum = sum + b
+
+    return sum & 0xFF
+    
+def sendPacket(byte_list):
+    # Compute checksum of the packet we're sending
+    # and set the checksum of the response to something impossible
+    sent_checksum = checksum(byte_list)
+    # print("OUT\t" + str(bytes(byte_list)) + " * " + str(sent_checksum))
+    resp_checksum = -1
+
+    while(resp_checksum != sent_checksum):
+        port.write(bytearray(byte_list))
+        response = list(port.read(8))
+        resp_checksum = response[0]
+    
+    # print("IN\t" + str(bytes(response)))
+    return response
+
 # Send an output state to the device
+# Return true if the write is successful
 def digitalWrite(pin, state,delay):
     # Signal output state write
     packet = [0x02]
@@ -24,34 +47,30 @@ def digitalWrite(pin, state,delay):
 
     # Zero pad packet
     packet.extend([0])
-    # print(sched_item_bytelist)
-    port.write(bytearray(packet))
+    resp = sendPacket(packet)
+    return resp[1] == 0
 
-# Request items left 
+# Request items left
+# Return number of items that can be sent
 def requestItemsLeft():
     # Request number of items that can be written
-    port.write(bytearray([0x01,0x01,0,0,0,0,0,0]))
+    resp = sendPacket([0x01,0x01,0,0,0,0,0,0])
+
     # Read bytes in from the serial port and
     # Convert the bytes into an unsigned integer
-    items_left_bytes = port.read(2)
+    items_left_bytes = resp[1:3]
     return int.from_bytes(items_left_bytes, byteorder='big', signed=False)
 
 # Calculate counter value for the MCU
 def calcDelay(seconds):
-    if(seconds > MAX_TIME): return 0xFFFFFFFF
+    if(seconds > MAX_TIME):
+        print("ERR: Gave time more than MAX_TIME")
+        return 0xFFFFFFFF
     return int(seconds/TIME_PER_CYCLE)
 
-def sendCMD(cmd_byte):
-    port.write()
 
-# TEST BLINK ##
-def loop():
-    items_left = requestItemsLeft()
-    state = 1
-    while(items_left > 0):
-        digitalWrite(LED_BUILTIN, state, 0xFFFF)
-        items_left = items_left - 1
-        state = not state
-
+led_pin_state = 0
 while(True):
-    loop()
+    while(requestItemsLeft() > 0):
+        digitalWrite(LED_BUILTIN, led_pin_state,calcDelay(0.1))
+        led_pin_state = not led_pin_state
